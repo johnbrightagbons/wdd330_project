2d2e6db38e191b6ff0bfd7f5bd8313e59ae351ec
John Bright Agbons <brightinojohnny@gmail.com>
2025-06-18 23:19:15 +0100

    report

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         login: resolve(__dirname, "src/auth/login.html"),
         signup: resolve(__dirname, "src/auth/signup.html"),
-        budget: resolve(__dirname, "src/reports/report.html"),
+        report: resolve(__dirname, "src/reports/report.html"),
         budget: resolve(__dirname, "src/budget/index.html"),
         transactions: resolve(__dirname, "src/transactions/transaction.html"),
         // report: resolve(__dirname, "src/reports/report.html"),
